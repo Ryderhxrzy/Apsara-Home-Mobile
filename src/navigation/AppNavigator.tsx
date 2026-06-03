66f0c8cd927dee0053dadf707eda428420032868
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-06-03 09:47:40 +0800

    added wishlist items

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1197,6 +1197,8 @@ export default function AppNavigator({ user, token, onLogout, productSlugFromDee
               onShowAFWalletRewards={() => setShowAFWalletRewards(true)}
               onShowAFWalletNetwork={() => setShowAFWalletNetwork(true)}
               onShowPVEarner={(show) => setShowPVEarnerFromTab(show)}
+              wishlistItems={wishlistItems}
+              onWishlistChange={invalidateWishlist}
             />
           ) : activeTab === 'shop' ? (
             selectedBrandId != null && selectedBrand ? (
